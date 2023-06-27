2c397f6c1819c4e7be09dab899e2b1613728d843
ge-arty <george.arutinyan@gmail.com>
2023-06-27 14:25:08 +0400

    Saving states in localstorage and Token in Cookies

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./containers/Header";
 import Content from "./containers/Content";
 import Footer from "./containers/Footer";
-import Cookies from "js-cookie";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import useLocalStorage from "./hooks/useLocalStorage";
+import useCookiesStorage from "./hooks/useCookiesStorage";
 
 function App() {
-  const [isLogged, setIsLogged] = useState(false);
-  const [loggedUserId, setLoggedUserId] = useState("");
-  const [token, setToken] = useState("");
+  const [isLogged, setIsLogged] = useLocalStorage("isLogged", false);
+  const [loggedUserId, setLoggedUserId] = useLocalStorage("loggedUserId", "");
+  const [token, setToken] = useCookiesStorage("token", "");
 
   useEffect(() => {
-    const savedIsLogged = localStorage.getItem("isLogged");
-    const savedLoggedUserId = localStorage.getItem("loggedUserId");
-    setIsLogged(savedIsLogged === "true");
-    setLoggedUserId(savedLoggedUserId);
-
-    // Token
-    const savedToken = Cookies.get("token");
-    if (savedToken === " ") setIsLogged(false);
-    if (savedToken) {
-      setToken((prevToken) => {
-        // Check if prev Token is not Saved One!
-        if (savedToken !== prevToken) {
-          Cookies.set("token", savedToken);
-          return savedToken;
-        }
-        return prevToken;
-      });
+    if (!token) {
+      setLoggedUserId("");
+      setIsLogged(false);
     }
   }, []);
-  console.log(token);
 
   return (
     <div className="app vh-100 bg-dark d-flex justify-content-between flex-column">
