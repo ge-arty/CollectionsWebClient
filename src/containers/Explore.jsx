668548cb7e51c977345d765398bcf6a01c16a59d
ge-arty <george.arutinyan@gmail.com>
2023-06-28 16:10:27 +0400

    Created Explore Page

--- src/containers/Explore.jsx
+++ src/containers/Explore.jsx
@@ -1,5 +1,36 @@
 import React from "react";
 
 export default function Explore() {
-  return <div className="container">Explore</div>;
+  const latestCollections = [
+    { id: 1, title: "Collection 1" },
+    { id: 2, title: "Collection 2" },
+    { id: 3, title: "Collection 3" },
+    { id: 4, title: "Collection 4" },
+    { id: 5, title: "Collection 5" },
+  ];
+
+  return (
+    <div className="container">
+      <div className="text-center my-5">
+        <h1 className="display-4" style={{ color: "#fff" }}>
+          Сreate your own collection and join our big community!
+        </h1>
+      </div>
+
+      <section>
+        <h2 className="mb-4" style={{ color: "#fff" }}>
+          Latest updates
+        </h2>
+        <div className="card-deck">
+          {latestCollections.map((collection) => (
+            <div key={collection.id} className="card mb-4">
+              <div className="card-body">
+                <h3 className="card-title">{collection.title}</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
